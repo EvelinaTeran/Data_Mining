da6145a14f5aece4f373921bebeaa2146aafc9df
EvelinaTeran <156710266+EvelinaTeran@users.noreply.github.com>
2024-01-21 22:15:55 -0500

    Update question2.py

--- question2.py
+++ question2.py
@@ -1,5 +1,5 @@
 def question2_1():
-    answer = ['discrete', 'quantitative', 'interval']
+    answer = ['discrete', 'quantitative', 'ratio']
     return answer
 
 def question2_2():
@@ -7,7 +7,7 @@ def question2_2():
     return answer
 
 def question2_3():
-    answer = ['continuous', 'quantitative', 'ratio']
+    answer = ['continuous', 'quantitative', 'interval']
     return answer 
 
 def question2_4():
