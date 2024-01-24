ff9626965504e92e0656d56232ea8098f0e668d3
EvelinaTeran <156710266+EvelinaTeran@users.noreply.github.com>
2024-01-24 17:42:09 -0500

    Update question5.py

--- question5.py
+++ question5.py
@@ -1,15 +1,17 @@
 def question5_1():
-    """
     answer = {
-            'bin1': [d1,d2,d3...]
-            'bin2': [e1,...],
-            'bin3': [f1,...]
+            'bin1': [1]
+            'bin2': [2, 3, 4, 5],
+            'bin3': [6, 7, 8, 9]
     }
-    where di, ei, fi are integers. 
-    """
     return answer
 
 def question5_2():
+    answer = {
+            'bin1': [1, 2, 3]
+            'bin2': [4, 5, 6,],
+            'bin3': [7, 8, 9]
+    }
     return answer
 
 def question5_3():
