907325fd7c4e9530b5ba8dc0bb3a15047054a76d
EvelinaTeran <156710266+EvelinaTeran@users.noreply.github.com>
2024-01-29 23:24:22 -0500

    Update question5.py

--- question5.py
+++ question5.py
@@ -1,23 +1,23 @@
 def question5_1():
     answer = {
-            'bin1': [1]
-            'bin2': [2, 3, 4, 5],
-            'bin3': [6, 7, 8, 9]
+            'bin1': [1, 2, 3, 4]
+            'bin2': [5, 6, 7],
+            'bin3': [8, 9]
     }
     return answer
 
 def question5_2():
     answer = {
             'bin1': [1, 2, 3]
-            'bin2': [4, 5, 6,],
+            'bin2': [4, 5, 6],
             'bin3': [7, 8, 9]
     }
     return answer
 
 def question5_3():
     answer = {
-    'bin1': [1,5,6,7],
-    'bin2': [2,3,4,8,9],
-    'bin3': []
+    'bin1': [1,2,3,4],
+    'bin2': [5,6,7],
+    'bin3': [8,9]
 }
     return answer 
