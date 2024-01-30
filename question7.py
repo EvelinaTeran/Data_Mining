8b7a075be13d92f37fdf5954c95651a538ed298a
EvelinaTeran <156710266+EvelinaTeran@users.noreply.github.com>
2024-01-29 23:02:13 -0500

    Update question7.py

--- question7.py
+++ question7.py
@@ -1,18 +1,14 @@
 def question7_1():
-    """
-    answer = 'string'
-    """
+    answer = 'increase/decrease'
     return answer
 
 def question7_2():
-    """
-    answer = 'string'
-    """
+    answer = 'increase/decrease'
     return answer
 
 def question7_3():
+    answer = [(0.238, 0.429), (0.429, 0.629), ..., ('infinity', 'infinity')]
     """
-    answer = [(b1, e1), (b2, e2), ..., (bn, en)]
     The representation of inifinity is 'infinity'. 
     bi, ei are floats or 'infinity'. 
     """
