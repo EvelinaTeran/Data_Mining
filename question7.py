64f41dff17a0b6bc2974efdbd9bf68b91cdd70cf
EvelinaTeran <156710266+EvelinaTeran@users.noreply.github.com>
2024-01-30 11:35:20 -0500

    Update question7.py

--- question7.py
+++ question7.py
@@ -8,8 +8,8 @@ def question7_2():
 
 def question7_3():
     answer = [(0.238, 0.429), (0.429, 0.629), ..., ('infinity', 'infinity')]
-    """
-    The representation of inifinity is 'infinity'. 
-    bi, ei are floats or 'infinity'. 
-    """
+    
+    # The representation of inifinity is 'infinity'. 
+    # bi, ei are floats or 'infinity'. 
+    
     return answer 
